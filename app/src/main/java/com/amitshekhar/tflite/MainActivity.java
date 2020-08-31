089c2800428dd20b69a7889926ed52250fc67278
Nacrow <529944053@qq.com>
2020-08-31 11:05:04 +0800

    Final demo

--- app/src/main/java/com/amitshekhar/tflite/MainActivity.java
+++ app/src/main/java/com/amitshekhar/tflite/MainActivity.java
@@ -1,14 +1,31 @@
 package com.amitshekhar.tflite;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.wonderkiln.camerakit.CameraKit;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
 import com.wonderkiln.camerakit.CameraKitEventListener;
@@ -16,16 +33,32 @@ import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import static android.hardware.Camera.Parameters.FOCUS_MODE_AUTO;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String MODEL_PATH = "model-export_icn_tflite-EdgeCV.tflite";
+    private static final String MODEL_PATH = "model-export_icn_tflite-EdgeCV_final_proj.tflite";
     private static final boolean QUANT = true;
     private static final String LABEL_PATH = "labels.txt";
     private static final int INPUT_SIZE = 224;
+
+
+    private static final int TAKE_PHOTO_PERMISSION_REQUEST_CODE = 0; // 拍照的权限处理返回码
+    private static final int WRITE_SDCARD_PERMISSION_REQUEST_CODE = 1; // 读储存卡内容的权限处理返回码
+
+    private static final int TAKE_PHOTO_REQUEST_CODE = 3; // 拍照返回的 requestCode
+    private static final int CHOICE_FROM_ALBUM_REQUEST_CODE = 4; // 相册选取返回的 requestCode
+    private static final int CROP_PHOTO_REQUEST_CODE = 5; // 裁剪图片返回的 requestCode
+
+    private Uri photoUri = null;
+    private Uri photoOutputUri = null; // 图片最终的输出文件的 Uri
 
     private Classifier classifier;
 
@@ -34,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnDetectObject, btnToggleCamera;
     private ImageView imageViewResult;
     private CameraView cameraView;
+    Button take_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +80,22 @@ public class MainActivity extends AppCompatActivity {
 
         btnToggleCamera = findViewById(R.id.btnToggleCamera);
         btnDetectObject = findViewById(R.id.btnDetectObject);
+        take_photo=findViewById(R.id.btnfile);
 
+        take_photo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // 申请读写内存卡内容的权限
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_SDCARD_PERMISSION_REQUEST_CODE);
+                }
+                choiceFromAlbum();
+
+            }
+        });
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
             public void onEvent(CameraKitEvent cameraKitEvent) {
@@ -60,16 +109,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
+                cameraView.setFocus(CameraKit.Constants.FOCUS_TAP);
 
                 Bitmap bitmap = cameraKitImage.getBitmap();
+                Uri uri = Uri.parse(MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, null,null));
+                cropPhoto(uri);
+                //bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+               // imageViewResult.setImageBitmap(bitmap);
+                //final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
 
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-
-                imageViewResult.setImageBitmap(bitmap);
-
-                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
-
-                textViewResult.setText(results.toString());
+                //textViewResult.setText(results.toString());
 
             }
 
@@ -146,4 +195,91 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void choiceFromAlbum() {
+        // 打开系统图库的 Action，等同于: "android.intent.action.GET_CONTENT"
+        Intent choiceFromAlbumIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        // 设置数据类型为图片类型
+        choiceFromAlbumIntent.setType("image/*");
+        startActivityForResult(choiceFromAlbumIntent, CHOICE_FROM_ALBUM_REQUEST_CODE);
+    }
+    private void cropPhoto(Uri inputUri) {
+        // 调用系统裁剪图片的 Action
+        Intent cropPhotoIntent = new Intent("com.android.camera.action.CROP");
+        // 设置数据Uri 和类型
+        cropPhotoIntent.setDataAndType(inputUri, "image/*");
+        // 授权应用读取 Uri，这一步要有，不然裁剪程序会崩溃
+        cropPhotoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        // 设置图片的最终输出目录
+        cropPhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                photoOutputUri = Uri.parse("file:////sdcard/image_output.jpg"));
+        startActivityForResult(cropPhotoIntent, CROP_PHOTO_REQUEST_CODE);
+    }
+
+    /**
+     * 在这里进行用户权限授予结果处理
+     * @param requestCode 权限要求码，即我们申请权限时传入的常量
+     * @param permissions 保存权限名称的 String 数组，可以同时申请一个以上的权限
+     * @param grantResults 每一个申请的权限的用户处理结果数组(是否授权)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            // 调用相机拍照：
+            case TAKE_PHOTO_PERMISSION_REQUEST_CODE:
+                // 如果用户授予权限，那么打开相机拍照
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //startCamera();
+                } else {
+                    Toast.makeText(this, "拍照权限被拒绝", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            // 打开相册选取：
+            case WRITE_SDCARD_PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "读写内存卡内容权限被拒绝", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 通过这个 activity 启动的其他 Activity 返回的结果在这个方法进行处理
+     * 我们在这里对拍照、相册选择图片、裁剪图片的返回结果进行处理
+     * @param requestCode 返回码，用于确定是哪个 Activity 返回的数据
+     * @param resultCode 返回结果，一般如果操作成功返回的是 RESULT_OK
+     * @param data 返回对应 activity 返回的数据
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            // 通过返回码判断是哪个应用返回的数据
+            switch (requestCode) {
+                // 拍照
+                case TAKE_PHOTO_REQUEST_CODE:
+                    cropPhoto(photoUri);
+                    break;
+                // 相册选择
+                case CHOICE_FROM_ALBUM_REQUEST_CODE:
+                    cropPhoto(data.getData());
+                    break;
+                // 裁剪图片
+                case CROP_PHOTO_REQUEST_CODE:
+                    File file = new File(photoOutputUri.getPath());
+                    if(file.exists()) {
+                        Bitmap bitmap = BitmapFactory.decodeFile(photoOutputUri.getPath());
+                        initTensorFlowAndLoadModel();
+                        imageViewResult.setImageBitmap(bitmap);
+                        bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                        final List<Classifier.Recognition> results2 = classifier.recognizeImage(bitmap);
+                        textViewResult.setText(results2.toString());
+//                        file.delete(); // 选取完后删除照片
+                    } else {
+                        Toast.makeText(this, "找不到照片", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    }
 }
+
